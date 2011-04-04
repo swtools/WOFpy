fdce6a8367370ba2aa35319137ad5291d20db8dc
James Seppi <james.seppi@gmail.com>
2011-04-04 13:17:18 -0500

    added swis2.db. wrote more unittests.

--- Test_OdmSqlAlchDao.py
+++ Test_OdmSqlAlchDao.py
@@ -71,11 +71,10 @@ class Test_OdmSqlAlchDao(unittest.TestCase):
         
     def test_get_all_sites(self):
         siteResultList = self.dao.get_all_sites()
-        resultSiteCodes = [s.SiteCode for s in siteResultList ]
+        resultSiteCodes = [s.SiteCode for s in siteResultList]
         for known_code in self.known_site_codes:
             self.assertTrue(known_code in resultSiteCodes)
-        
-        
+          
     def test_get_site_by_code(self):
         for known_code in self.known_site_codes:
             siteResult = self.dao.get_site_by_code(known_code)
@@ -83,8 +82,23 @@ class Test_OdmSqlAlchDao(unittest.TestCase):
         
     def test_get_sites_by_codes(self):
         siteResultList = self.dao.get_sites_by_codes(self.known_site_codes)
-        resultSiteCodes = [s.SiteCode for s in siteResultList ]
+        resultSiteCodes = [s.SiteCode for s in siteResultList]
         for known_code in self.known_site_codes:
             self.assertTrue(known_code in resultSiteCodes)
     
+    def test_get_all_variables(self):
+        varResultList = self.dao.get_all_variables()
+        resultVarCodes = [v.VariableCode for v in varResultList]
+        for known_code in self.known_var_codes:
+            self.assertTrue(known_code in resultVarCodes)
     
+    def test_get_var_by_code(self):
+        for known_code in self.known_var_codes:
+            varResult = self.dao.get_variable_by_code(known_code)
+            self.assertEqual(known_code, varResult.VariableCode)
+            
+    def test_get_vars_by_codes(self):
+        varResultList = self.dao.get_variables_by_codes(self.known_var_codes)
+        resultVarCodes = [v.VariableCode for v in varResultList]
+        for known_code in self.known_var_codes:
+            self.assertTrue(known_code in resultVarCodes)
