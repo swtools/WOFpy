e72201211139ecb75154f7958ccc421199ca2a5d
James Seppi <james.seppi@gmail.com>
2011-05-05 14:38:31 -0500

    started cbi test classes

--- WOFpy/test/test_cbi_parser.py
+++ WOFpy/test/test_cbi_parser.py
@@ -1,10 +1,28 @@
 import unittest
+import os
 
-import daos.cbi.cbi_sos_parser
+from lxml import etree
+
+from daos.cbi.cbi_sos_parser import parse_datavalues_from_get_observation
+
 
 class TestCbiParser(unittest.TestCase):
     def setUp(self):
         pass
     
     def test_parse_datavalues_from_get_observation(self):
-        pass
+        
+        path = os.path.join(os.path.dirname(__file__), 'cbi_sos_examples',
+                     'cbi_get_observations.xml')
+        
+        f = open(path)
+        tree = etree.parse(f)
+        
+        dataval_list = \
+            parse_datavalues_from_get_observation(tree, '014',
+                                                  'water_temperature')
+        
+        self.assertTrue(dataval_list)
+        
+        for datavalue in dataval_list:
+            self.assertTrue(datavalue.DataValue)
