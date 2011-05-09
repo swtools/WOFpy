4b3a448e10627ecb0f2acc2991ba7e9ca1a2f245
James Seppi <james.seppi@gmail.com>
2011-05-09 10:56:38 -0500

    finished get_variable dao methods for cbi. started test class for cbi dao.

--- WOFpy/daos/cbi/cbi_dao.py
+++ WOFpy/daos/cbi/cbi_dao.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from daos.base_dao import BaseDao
 
 import wof
-import cbi_cache_models as cache_model
+import cbi_cache_models as cache
 import cbi_models as model
 import cbi_sos_client
 import cbi_sos_parser
@@ -19,7 +19,7 @@ class CbiDao(BaseDao):
         self.engine = create_engine(db_connection_string, convert_unicode=True)
         self.db_session = scoped_session(sessionmaker(
             autocommit=False, autoflush=False, bind=self.engine))
-        cache_model.init_model(self.db_session)
+        cache.init_model(self.db_session)
         
         self.cbi_sos_client = cbi_sos_client.CbiSosClient(
             'http://lighthouse.tamucc.edu/sos')
@@ -28,44 +28,43 @@ class CbiDao(BaseDao):
         """
         Returns a list of all the Sites in the CBI site cache.
         """
-        return cache_model.Site.query.all()
+        return cache.Site.query.all()
 
     def get_site_by_code(self, site_code):
         """
         Returns a single Site identified by its code.
         """        
-        return cache_model.Site.query.filter(
-            cache_model.Site.SiteCode==site_code).first()
+        return cache.Site.query.filter(
+            cache.Site.SiteCode==site_code).first()
 
 
     def get_sites_by_codes(self, site_codes_arr):
         """
         Returns a list of Sites identified by the given site code list.
         """
-        return cache_model.Site.query.filter(
-            cache_model.Site.SiteCode.in_(site_codes_arr)).all()
+        return cache.Site.query.filter(
+            cache.Site.SiteCode.in_(site_codes_arr)).all()
 
     def get_all_variables(self):
         """
         Returns a list of all Variables in the data source.
         """
+        return cache.Variable.query.all()
         
-        #TODO: Not sure where to get all the variable info
-        pass
 
     def get_variable_by_code(self, var_code):
         """
         Returns a single Variable identified by its code.
         """
-        #TODO: Not sure where to get all the variable info
-        return model.Variable(var_code, var_code, var_code)
+        return cache.Variable.query.filter(
+            cache.Variable.VariableCode == var_code).first()
 
     def get_variables_by_codes(self, var_codes_arr):
         """
         Returns a list of Variables identified by the given variable code list.
         """
-        #TODO: Not sure where to get all the variable info
-        pass
+        return cache.Variable.query.filter(
+            cache.Variable.VariableCode.in_(var_codes_arr)).all()
 
     def get_series_by_sitecode(self, site_code):
         """
