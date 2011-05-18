7b08c4656d9d84302a5722ca9dcdef6a37a9eae8
James Seppi <james.seppi@gmail.com>
2011-05-18 13:36:06 -0500

    small cleanups

--- WOFpy/wofpy_soap/soap.py
+++ WOFpy/wofpy_soap/soap.py
@@ -10,7 +10,6 @@ from soaplib.core.model.primitive import String, Any, Integer, Float
 from soaplib.core.model.exception import Fault
 from soaplib.core.model.clazz import Array, ClassModel
 
-
 logger = logging.getLogger(__name__)
 
 NSDEF = 'xmlns:gml="http://www.opengis.net/gml" \
@@ -61,13 +60,14 @@ def create_wof_service_class(wof_instance):
             
             return str(outStream.getvalue()).replace('\n','')
         
-        ###########################################################################
+        #######################################################################
         
         @soap(String, String, _returns=String)
         def GetSiteInfo(self,site,authToken):
             
             try:
-                siteInfoResponse = self.wof_inst.create_get_site_info_response(site)
+                siteInfoResponse = \
+                            self.wof_inst.create_get_site_info_response(site)
                 
                 outStream = StringIO.StringIO()
                 siteInfoResponse.export(outStream, 0, name_="siteInfoResponse",
@@ -98,7 +98,7 @@ def create_wof_service_class(wof_instance):
             except Exception as inst:
                 return "ERROR: %s, %s" % (type(inst), inst)
         
-        ###########################################################################
+        #######################################################################
         
         @soap(String, String, _returns=String)
         def GetVariableInfo(self, variable, authToken):
@@ -107,7 +107,8 @@ def create_wof_service_class(wof_instance):
                     self.wof_inst.create_get_variable_info_response(variable)
             
             outStream = StringIO.StringIO()
-            variableInfoResponse.export(outStream, 0, name_="variablesResponse",
+            variableInfoResponse.export(outStream, 0,
+                                        name_="variablesResponse",
                                         namespacedef_= NSDEF)
             
             #TODO: Fault
@@ -121,14 +122,15 @@ def create_wof_service_class(wof_instance):
                     self.wof_inst.create_get_variable_info_response(variable)
             
             outStream = StringIO.StringIO()
-            variableInfoResponse.export(outStream, 0, name_="variablesResponse",
+            variableInfoResponse.export(outStream, 0,
+                                        name_="variablesResponse",
                                         namespacedef_= NSDEF)
             
             #TODO: Fault
             
             return outStream.getvalue()
         
-        ###########################################################################
+        #######################################################################
     
         @soap(String, String, String, String, _returns=String)
         def GetValues(self, location, variable, startDate, endDate):
