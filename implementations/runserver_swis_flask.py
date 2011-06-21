e2d28eff7aa13554a199189f2aa2f0d21156252a
wilsaj <wilson.andrew.j+github@gmail.com>
2011-06-21 15:43:43 -0500

    fix imports inswis example

--- implementations/runserver_swis_flask.py
+++ implementations/runserver_swis_flask.py
@@ -1,26 +1,24 @@
-
 import soaplib
 import logging
 
 from werkzeug.wsgi import DispatcherMiddleware
 from soaplib.core.server import wsgi
 
-import wofpy_soap
+import wof.soap
 import private_config
 
 from wof import WOF
-from wofpy_soap import create_wof_service_class
-from wofpy_flask import config
-from wofpy_flask import create_app
+from wof.soap import create_wof_service_class
+from wof.flask import create_app, config
 from daos.swis.swis_dao import SwisDao
 
 logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == '__main__':
-    
+
     dao = SwisDao(private_config.swis_connection_string,
                   'config/swis_config.cfg')
-    
+
     swis_wof = WOF(dao)
     swis_wof.config_from_file('config/swis_config.cfg')
 
@@ -28,7 +26,7 @@ if __name__ == '__main__':
     flask_app.config.from_object(config.DevConfig)
 
     SWISWOFService = create_wof_service_class(swis_wof)
-    
+
     soap_app = soaplib.core.Application(services=[SWISWOFService],
         tns='http://www.cuahsi.org/his/1.0/ws/',
         name='WaterOneFlow')
