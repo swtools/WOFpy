654bdb4ffd2f276df1ef4a16ca20978260a52fa2
wilsaj <wilson.andrew.j+github@gmail.com>
2011-06-21 16:05:35 -0500

    update runserver_lbr.py and runserver_sara.py examples to use new wof namespace

--- implementations/runserver_lbr.py
+++ implementations/runserver_lbr.py
@@ -1,28 +1,25 @@
-
 import soaplib
 import logging
 
 from werkzeug.wsgi import DispatcherMiddleware
 from soaplib.core.server import wsgi
 
-import wofpy_soap
 import private_config
 
 from wof import WOF
-from wofpy_soap import create_wof_service_class
-from wofpy_flask import config
-from wofpy_flask import create_app
+from wof.soap import create_wof_service_class
+from wof.flask import config
+from wof.flask import create_app
 from daos.odm.odm_dao import OdmDao
 
 logging.basicConfig(level=logging.DEBUG)
 
-if __name__ == '__main__':
 
-    
+if __name__ == '__main__':
     dao = OdmDao(private_config.lbr_connection_string)
     odm_wof = WOF(dao)
     odm_wof.config_from_file('config/lbr_config.cfg')
-    
+
     flask_app = create_app(odm_wof)
     flask_app.config.from_object(config.DevConfig)
 
@@ -35,7 +32,7 @@ if __name__ == '__main__':
     soap_wsgi_app = soaplib.core.server.wsgi.Application(soap_app)
 
     flask_app.wsgi_app = DispatcherMiddleware(flask_app.wsgi_app, {
-        '/soap/lbr': soap_wsgi_app,        
+        '/soap/lbr': soap_wsgi_app,
     })
 
     flask_app.run(host='0.0.0.0', port=8080, threaded=True)
