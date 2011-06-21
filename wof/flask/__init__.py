cd5cbc908e4f60e848c2a90a9c390ccbdb4a5625
wilsaj <wilson.andrew.j+github@gmail.com>
2011-06-21 15:40:36 -0500

    change imports from wofpy_flask to wof.flask

--- wof/flask/__init__.py
+++ wof/flask/__init__.py
@@ -1,19 +1,17 @@
 from flask import Flask, render_template
 
 import config
+from wof.flask.views.rest import rest
+from wof.flask.views.wsdl import wsdl
+
 
 #TODO: Error handlers (404, etc.)
-
 def create_app(wof_inst):
     app = Flask(__name__)
     app.config.from_object(config.Config)
 
     app.wof_inst = wof_inst
-
-    from wofpy_flask.views.rest import rest
     app.register_module(rest)
-    
-    from wofpy_flask.views.wsdl import wsdl
     app.register_module(wsdl)
-    
+
     return app
