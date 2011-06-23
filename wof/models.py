45d1031c122b2bafd973fb0f34dd5ae86b3b0635
wilsaj <wilson.andrew.j+github@gmail.com>
2011-06-23 12:07:58 -0500

    whitespace + pep8 + misc cleanup

--- wof/models.py
+++ wof/models.py
@@ -1,53 +1,52 @@
-
-
-
 class SampleMediumTypes:
-    SURFACE_WATER='Surface Water'
-    GROUND_WATER='Ground Water'
-    SEDIMENT='Sediment'
-    SOIL='Soil'
-    AIR='Air'
-    TISSUE='Tissue'
-    PRECIPITATION='Precipitation'
-    UNKNOWN='Unknown'
-    OTHER='Other'
-    SNOW='Snow'
-    NOT_RELEVANT='Not Relevant'
-    
-class GeneralCategoryTypes:
-    WATER_QUALITY='Water Quality'
-    CLIMATE='Climate'
-    HYDROLOGY='Hydrology'
-    GEOLOGY='Geology'
-    BIOTA='Biota'
-    UNKNOWN='Unknown'
-    INSTRUMENTATION='Instrumentation'
+    SURFACE_WATER = 'Surface Water'
+    GROUND_WATER = 'Ground Water'
+    SEDIMENT = 'Sediment'
+    SOIL = 'Soil'
+    AIR = 'Air'
+    TISSUE = 'Tissue'
+    PRECIPITATION = 'Precipitation'
+    UNKNOWN = 'Unknown'
+    OTHER = 'Other'
+    SNOW = 'Snow'
+    NOT_RELEVANT = 'Not Relevant'
 
-QualityControlLevelTypes = dict(
-    UNKNOWN=('Unknown',-9999),
-    RAW_DATA=('Raw data',0),
-    QUAL_CONTROLLED_DATA=('Quality controlled data',1),
-    DERIVED_PRODUCTS=('Derived products',2),
-    INTERPRETED_PRODUCTS=('Interpreted products',3),
-    KNOWLEDGE_PRODUCTS=('Knowledge products',4)
-)
+
+class GeneralCategoryTypes:
+    WATER_QUALITY = 'Water Quality'
+    CLIMATE = 'Climate'
+    HYDROLOGY = 'Hydrology'
+    GEOLOGY = 'Geology'
+    BIOTA = 'Biota'
+    UNKNOWN = 'Unknown'
+    INSTRUMENTATION = 'Instrumentation'
+
+
+QualityControlLevelTypes = {
+    'UNKNOWN': ('Unknown', -9999),
+    'RAW_DATA': ('Raw data', 0),
+    'QUAL_CONTROLLED_DATA': ('Quality controlled data', 1),
+    'DERIVED_PRODUCTS': ('Derived products', 2),
+    'INTERPRETED_PRODUCTS': ('Interpreted products', 3),
+    'KNOWLEDGE_PRODUCTS': ('Knowledge products', 4)
+}
+
 
 class BaseUnits(object):
-    
     UnitsID = None
     UnitsName = None
     UnitsType = None
     UnitsAbbreviation = None
-    
+
+
 class BaseSpatialReference(object):
-    
     SpatialReferenceId = None
     SRSID = None
     SRSName = None
     Notes = None
 
+
 class BaseVariable(object):
-    
     VariableID = None
     VariableCode = None
     VariableName = None
@@ -61,32 +60,32 @@ class BaseVariable(object):
     GeneralCategory = None
     NoDataValue = None
     VariableDescription = None
-    
     VariableUnits = BaseUnits()
     TimeUnits = BaseUnits()
 
-class BaseSite(object):
 
+class BaseSite(object):
     SiteID = None
     SiteCode = None
     SiteName = None
     Latitude = None
     Longitude = None
-    LatLongDatumID = None #FK to SpatialReferences
+    #FK to SpatialReferences
+    LatLongDatumID = None
     Elevation_m = None
     VerticalDatum = None
     LocalX = None
     LocalY = None
-    LocalProjectionID = None #FK to SpatialReferences
+    #FK to SpatialReferences
+    LocalProjectionID = None
     State = None
     County = None
     Comments = None
-    
     LatLongDatum = BaseSpatialReference()
     LocalProjection = BaseSpatialReference()
-    
+
+
 class BaseDataValue(object):
-    
     ValueID = None
     DataValue = None
     ValueAccuracy = None
@@ -94,7 +93,7 @@ class BaseDataValue(object):
     UTCOffset = None
     DateTimeUTC = None
     SiteID = None
-    VariableID = None 
+    VariableID = None
     OffsetValue = None
     OffsetTypeID = None
     CensorCode = None
@@ -104,43 +103,41 @@ class BaseDataValue(object):
     SampleID = None
     QualityControlLevel = None
     QualityControlLevelID = None
-    
+
     def __repr__(self):
         return '<DataValue: (%s, %s)>' % (self.DataValue, self.DateTimeUTC)
-    
+
 
 class BaseQualifier(object):
-    
     QualifierID = None
     QualifierCode = None
     QualifierDescription = None
 
+
 class BaseOffsetType(object):
-    
     OffsetTypeID = None
     OffsetUnitsID = None
     OffsetDescription = None
-    
+
     OffsetUnits = BaseUnits()
 
+
 class BaseMethod(object):
- 
     MethodID = None
     MethodDescription = None
     MethodLink = None
-  
+
+
 class BaseMetadata(object):
-    
     MetadataID = None
     TopicCategory = None
     Title = None
     Abstract = None
     ProfileVersion = None
     MetadataLink = None
-    
-    
-class BaseSource(object):
 
+
+class BaseSource(object):
     SourceID = None
     Organization = None
     SourceDescription = None
@@ -153,15 +150,15 @@ class BaseSource(object):
     State = None
     ZipCode = None
     MetadataID = None
-    
     Metadata = BaseMetadata()
-    
+
+
 class BaseQualityControlLevel(object):
     QualityControlLevelID = None
     QualityControlLevelCode = None
 
+
 class BaseSeriesCatalog(object):
-    
     SeriesID = None
     SiteID = None
     SiteCode = None
@@ -169,7 +166,7 @@ class BaseSeriesCatalog(object):
     VariableID = None
     VariableCode = None
     VariableName = None
-    VariableUnitsID = None 
+    VariableUnitsID = None
     VariableUnitsName = None
     SampleMedium = None
     ValueType = None
@@ -190,15 +187,13 @@ class BaseSeriesCatalog(object):
     BeginDateTimeUTC = None
     EndDateTimeUTC = None
     ValueCount = None
-    
+
     Site = BaseSite()
     Variable = BaseVariable()
     Method = BaseMethod()
-    
-    
+
+
 class BaseVerticalDatum(object):
-    
+
     Term = None
     Definition = None
-    
-    
