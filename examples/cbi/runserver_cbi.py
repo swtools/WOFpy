0465965f0a6deb021500a36f60cb7726002f5b0c
wilsaj <wilson.andrew.j+github@gmail.com>
2011-06-27 15:57:53 -0500

    update cbi example's runserver_cbi.py to be more production-like

--- examples/cbi/runserver_cbi.py
+++ examples/cbi/runserver_cbi.py
@@ -13,15 +13,21 @@ from wof.flask import create_app
 from cbi_dao import CbiDao
 
 
+# change the deployment dir if you are going to deploy this in production
+CBI_DEPLOYMENT_DIR = tempfile.gettempdir()
+CBI_CACHE_DIR = os.path.join(CBI_DEPLOYMENT_DIR,
+                             'cache/')
+CBI_CONFIG_FILE = os.path.join(CBI_DEPLOYMENT_DIR,
+                               'cbi_config.cfg')
 CBI_CACHE_DATABASE_URI = 'sqlite:////' + os.path.join(
-    tempfile.gettempdir(), 'cbi_dao_cache.db')
+    CBI_CACHE_DIR, 'cbi_dao_cache.db')
 
 logging.basicConfig(level=logging.DEBUG)
 
 
-dao = CbiDao('cbi_config.cfg', CBI_CACHE_DATABASE_URI)
+dao = CbiDao(CBI_CONFIG_FILE, database_uri=CBI_CACHE_DATABASE_URI)
 cbi_wof = WOF(dao)
-cbi_wof.config_from_file('cbi_config.cfg')
+cbi_wof.config_from_file(CBI_CONFIG_FILE)
 
 app = create_app(cbi_wof)
 app.config.from_object(config.DevConfig)
@@ -35,7 +41,7 @@ soap_app = soaplib.core.Application(services=[CBIWOFService],
 soap_wsgi_app = soaplib.core.server.wsgi.Application(soap_app)
 
 app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
-    '/soap/cbi': soap_wsgi_app
+    '/soap/wateroneflow': soap_wsgi_app
     })
 
 if __name__ == '__main__':
