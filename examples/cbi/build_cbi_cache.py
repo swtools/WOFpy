635b16d628d75302c562d57add63f7ddc260fc4d
wilsaj <wilson.andrew.j+github@gmail.com>
2011-07-06 15:50:12 -0500

    update import for build_cbi_cache.py

--- examples/cbi/build_cbi_cache.py
+++ examples/cbi/build_cbi_cache.py
@@ -9,7 +9,7 @@ from lxml import etree
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-import daos.cbi.cbi_cache_models as model
+import cbi_cache_models as model
 
 IOOS_SITE_FILE_URL = 'http://lighthouse.tamucc.edu/ioosobsreg.xml'
 CBI_SOS_CAPABILITIES_URL = 'http://lighthouse.tamucc.edu/sos'
