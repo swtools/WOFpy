74864b122f2450b188f1dd158ac6d612f8c12a55
wilsaj <wilson.andrew.j+github@gmail.com>
2011-07-13 23:57:44 -0500

    use create_app pattern for runserver_test

--- test/runserver_test.py
+++ test/runserver_test.py
@@ -5,12 +5,14 @@ import wof
 
 from test_dao import TestDao
 
-
 logging.basicConfig(level=logging.DEBUG)
 
-test_dao = TestDao()
-app = wof.create_wof_app(test_dao, 'test_config.cfg')
-app.config['DEBUG'] = True
+
+def create_app():
+    test_dao = TestDao()
+    app = wof.create_wof_app(test_dao, 'test_config.cfg')
+    app.config['DEBUG'] = True
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(host='0.0.0.0', port=8080, threaded=True)
