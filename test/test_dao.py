912ecf626aec7a893ebb80dac65ef7508bba3416
Tim Whiteaker <twhit@mail.utexas.edu>
2011-07-19 15:04:13 -0500

    In TestWOF.compare_output_to_known_xml, changed 'response' input argument to StringIO to avoid hard-coding root element name as 'sitesResponse'.  However, I'd prefer something that actually compared XML instead of string.  Added rstrip() when comparing XML lines to account for differences in newline characters between the response and the valid file.  Added tests for create_get_variable_info_response.

--- test/test_dao.py
+++ test/test_dao.py
@@ -7,6 +7,18 @@ class TestSite(models.BaseSite):
             setattr(self, k, v)
 
 
+class TestVariable(models.BaseVariable):
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+
+class TestUnits(models.BaseUnits):
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+
 test_sites = {
     'SITE_A': TestSite(
         SiteID='1',
@@ -28,6 +40,84 @@ test_sites = {
         Longitude=10),
     }
 
+test_units = {
+    96: TestUnits(
+        UnitsID = 96,
+        UnitsName = 'degree celcius',
+        UnitsType = 'Temperature',
+        UnitsAbbreviation = 'degC'),
+    100: TestUnits(
+        UnitsID = 100,
+        UnitsName = 'second',
+        UnitsType = 'Time',
+        UnitsAbbreviation = 's'),
+    35: TestUnits(
+        UnitsID = 35,
+        UnitsName = 'cubic feet per second',
+        UnitsType = 'Flow',
+        UnitsAbbreviation = 'cfs'),
+    102: TestUnits(
+        UnitsID = 102,
+        UnitsName = 'minute',
+        UnitsType = 'Time',
+        UnitsAbbreviation = 'min'),
+    199: TestUnits(
+        UnitsID = 199,
+        UnitsName = 'milligrams per liter',
+        UnitsType = 'Concentration',
+        UnitsAbbreviation = 'mg/L')
+    }
+
+test_variables = {
+    'Temp': TestVariable(
+        VariableID = 1,
+        VariableCode = 'Temp',
+        VariableName = 'Temperature',
+        VariableUnitsID = 96,
+        VariableUnits = test_units[96],
+        SampleMedium = 'Surface Water',
+        ValueType = 'Field Observation',
+        IsRegular = True,
+        TimeSupport = 0,
+        TimeUnitsID = 100,
+        TimeUnits = test_units[100],
+        DataType = 'Continuous',
+        GeneralCategory = 'Water Quality',
+        NoDataValue = -9999,
+        VariableDescription = None),
+    'Flow': TestVariable(
+        VariableID = 2,
+        VariableCode = 'Flow',
+        VariableName = 'Discharge',
+        VariableUnitsID = 35,
+        VariableUnits = test_units[35],
+        SampleMedium = 'Surface Water',
+        ValueType = 'Field Observation',
+        IsRegular = True,
+        TimeSupport = 15,
+        TimeUnitsID = 102,
+        TimeUnits = test_units[102],
+        DataType = 'Average',
+        GeneralCategory = 'Hydrology',
+        NoDataValue = -9999,
+        VariableDescription = 'Average streamflow'),
+    'TP': TestVariable(
+        VariableID = 3,
+        VariableCode = 'TP',
+        VariableName = 'Phosphorus, total as P',
+        VariableUnitsID = 199,
+        VariableUnits = test_units[199],
+        SampleMedium = 'Surface Water',
+        ValueType = 'Sample',
+        IsRegular = False,
+        TimeSupport = 0,
+        TimeUnitsID = 100,
+        TimeUnits = test_units[100],
+        DataType = 'Sporadic',
+        GeneralCategory = 'Water Quality',
+        NoDataValue = -9999,
+        VariableDescription = None)
+    }
 
 class TestDao(dao.BaseDao):
     def get_all_sites(self):
@@ -35,3 +125,9 @@ class TestDao(dao.BaseDao):
 
     def get_sites_by_codes(self, site_codes):
         return [test_sites[site_code] for site_code in site_codes]
+
+    def get_all_variables(self):
+        return test_variables.values()
+
+    def get_variables_by_codes(self, variable_codes):
+        return [test_variables[variable_code] for variable_code in variable_codes]
--- test/test_wof.py
+++ test/test_wof.py
@@ -33,39 +33,80 @@ class TestWOF(unittest.TestCase):
         waterml_schema_doc = etree.parse(waterml_schema_path)
         self.waterml_schema = etree.XMLSchema(waterml_schema_doc)
 
-    def compare_output_to_known_xml(self, response, filename):
+    def response_to_StringIO(self, response, root_name):
+        """
+        Converts WOF method response to StringIO object.
+        """
+        response_output = StringIO.StringIO()
+        response.export(response_output, 0, name_=root_name,
+                        namespacedef_=NSDEF)
+        response_output.seek(0)
+        return response_output
+
+    def save_response(self, response, filename, root_name):
+        f = open(filename, 'w')
+        response.export(f, 0, name_=root_name,
+                        namespacedef_=NSDEF)
+        f.close()
+        
+    def compare_output_to_known_xml(self, response_string, filename):
         """
         Make sure response output is as expected. This tests that a
         response contains the same text as the filename.
         """
         out_file_path = os.path.join(os.path.dirname(__file__),
                                      TEST_XML_DIR, filename)
-        response_output = StringIO.StringIO()
-        response.export(response_output, 0, name_="sitesResponse",
-                        namespacedef_=NSDEF)
-        response_output.seek(0)
         # test each line in the response text equals each line in the file
         with open(out_file_path, 'rb') as valid_file:
-            for response_line, valid_line in itertools.izip(response_output, valid_file):
-                assert response_line == valid_line
-
+            for response_line, valid_line in itertools.izip(response_string, valid_file):
+                assert response_line.rstrip() == valid_line.rstrip()
 
     def test_get_all_sites(self):
-        get_site_response = self.wof_inst.create_get_site_response()
-        self.compare_output_to_known_xml(get_site_response,
+        response = self.wof_inst.create_get_site_response()
+        response_string = self.response_to_StringIO(response,
+                                                    'sitesResponse')
+        self.compare_output_to_known_xml(response_string,
                                          'get_all_sites.xml')
 
     def test_get_one_site(self):
-        get_site_response = self.wof_inst.create_get_site_response(
+        response = self.wof_inst.create_get_site_response(
             'TEST:SITE_A')
-        self.compare_output_to_known_xml(get_site_response,
+        response_string = self.response_to_StringIO(response,
+                                                    'sitesResponse')
+        self.compare_output_to_known_xml(response_string,
                                          'get_one_site.xml')
 
     def test_get_multiple_sites(self):
-        get_site_response = self.wof_inst.create_get_site_response(
+        response = self.wof_inst.create_get_site_response(
             'TEST:SITE_A,TEST:SITE_B')
-        self.compare_output_to_known_xml(get_site_response,
+        response_string = self.response_to_StringIO(response,
+                                                    'sitesResponse')
+        self.compare_output_to_known_xml(response_string,
                                          'get_multiple_sites.xml')
+
+    def test_get_all_variables(self):
+        response = self.wof_inst.create_get_variable_info_response()
+        response_string = self.response_to_StringIO(response,
+                                                    'variablesResponse')
+        self.save_response(response, 'allvar.xml', 'variablesResponse')
+        self.compare_output_to_known_xml(response_string,
+                                         'get_all_variables.xml')
+
+    def test_get_one_variable(self):
+        response = self.wof_inst.create_get_variable_info_response(
+            'TESTVocab:Temp')
+        response_string = self.response_to_StringIO(response,
+                                                    'variablesResponse')
+        self.compare_output_to_known_xml(response_string,
+                                         'get_one_variable.xml')
+
+    def test_get_multiple_variables(self):
+        response = self.wof_inst.create_get_variable_info_response(
+            'TESTVocab:Flow,TESTVocab:TP')
+        response_string = self.response_to_StringIO(response,
+                                                    'variablesResponse')
+        self.compare_output_to_known_xml(response_string,
+                                         'get_multiple_variables.xml')
 
 def suite():
     suite = unittest.TestSuite()
