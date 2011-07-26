6bb119fa8bc0ecf4c64a777bf1ee1a663f6229fc
Tim Whiteaker <twhit@mail.utexas.edu>
2011-07-27 05:35:53 +0800

    Revised and added readme files.  Removed ODM example from runserver_multiple since it requires SQL Server.  I thought a more platform independent example would be good.  Renamed odm example to odm_sqlserver.  We should add ODM examples for MySQL, SQLite, etc.

--- examples/odm_sqlserver/runserver_lbr.py
+++ examples/odm_sqlserver/runserver_lbr.py
@@ -13,4 +13,9 @@ if __name__ == '__main__':
     app = wof.create_wof_app(dao, 'lbr_config.cfg')
     app.config['DEBUG'] = True
 
+    print "-----------------------------------------------------------------"
+    print "Access 'REST' endpoints at http://127.0.0.1:8080/"
+    print "Access SOAP WSDLs at http://127.0.0.1:8080/soap/wateroneflow.wsdl"
+    print "-----------------------------------------------------------------"
+
     app.run(host='0.0.0.0', port=8080, threaded=True)
--- examples/runserver_multiple.py
+++ examples/runserver_multiple.py
@@ -14,7 +14,6 @@ from wof import WOF
 from wof.soap import create_wof_service_class
 from wof.flask import config, create_app
 
-from examples.odm.odm_dao import OdmDao
 from examples.swis.swis_dao import SwisDao
 from examples.cbi.cbi_dao import CbiDao
 
@@ -30,14 +29,9 @@ if __name__ == '__main__':
     CBI_CACHE_DATABASE_URI = 'sqlite:///' + os.path.join(
     CBI_CACHE_DIR, 'cbi_dao_cache.db')
     
-    lbr_dao = OdmDao(private_config.lbr_connection_string)
     swis_dao = SwisDao(private_config.swis_connection_string,
                         'swis/swis_config.cfg')    
     cbi_dao = CbiDao('cbi/cbi_config.cfg', database_uri=CBI_CACHE_DATABASE_URI)
-    sara_dao = OdmDao(private_config.sara_connection_string)
-
-    lbr_wof = WOF(lbr_dao)    
-    lbr_wof.config_from_file('odm/lbr_config.cfg')
 
     swis_wof = WOF(swis_dao)    
     swis_wof.config_from_file('swis/swis_config.cfg')
@@ -45,26 +39,15 @@ if __name__ == '__main__':
     cbi_wof = WOF(cbi_dao)        
     cbi_wof.config_from_file('cbi/cbi_config.cfg')
 
-    sara_wof = WOF(sara_dao)
-    sara_wof.config_from_file('odm/sara_config.cfg')
-
     #Create the Flask applications
-    lbr_flask_app = create_app(lbr_wof)
     swis_flask_app = create_app(swis_wof)
     cbi_flask_app = create_app(cbi_wof)
-    sara_flask_app = create_app(sara_wof)
 
     #Create the soaplib classes
-    LBRWOFService = create_wof_service_class(lbr_wof)
     SWISWOFService = create_wof_service_class(swis_wof)
     CBIWOFService = create_wof_service_class(cbi_wof)
-    SARAWOFService = create_wof_service_class(sara_wof)
 
     #Create the soaplib applications
-    lbr_soap_app = soaplib.core.Application(services=[LBRWOFService],
-        tns='http://www.cuahsi.org/his/1.0/ws/',
-        name='WaterOneFlow')
-
     swis_soap_app = soaplib.core.Application(services=[SWISWOFService],
         tns='http://www.cuahsi.org/his/1.0/ws/',
         name='WaterOneFlow')
@@ -73,31 +56,21 @@ if __name__ == '__main__':
         tns='http://www.cuahsi.org/his/1.0/ws/',
         name='WaterOneFlow')
     
-    sara_soap_app = soaplib.core.Application(services=[SARAWOFService],
-        tns='http://www.cuahsi.org/his/1.0/ws/',
-        name='WaterOneFlow')
-
     #Create WSGI apps from the soaplib applications
-    lbr_soap_wsgi_app = soaplib.core.server.wsgi.Application(lbr_soap_app)
     swis_soap_wsgi_app = soaplib.core.server.wsgi.Application(swis_soap_app)
     cbi_soap_wsgi_app = soaplib.core.server.wsgi.Application(cbi_soap_app)
-    sara_soap_wsgi_app = soaplib.core.server.wsgi.Application(sara_soap_app)
 
     combined_app = DispatcherMiddleware(NotFound, {
-        '/lbr': lbr_flask_app,
         '/swis': swis_flask_app,
         '/cbi': cbi_flask_app,
-        '/sara': sara_flask_app,
-        '/lbr/soap/wateroneflow': lbr_soap_wsgi_app,        
         '/swis/soap/wateroneflow': swis_soap_wsgi_app,
-        '/cbi/soap/wateroneflow': cbi_soap_wsgi_app,
-        '/sara/soap/wateroneflow': sara_soap_wsgi_app
+        '/cbi/soap/wateroneflow': cbi_soap_wsgi_app
     })
     
     print "------------------------------------------------------------"
     print "Access 'REST' endpoints at http://HOST:8080/<network>"
     print "Access SOAP WSDLs at http://HOST:8080/<network>/soap/wateroneflow.wsdl"
-    print "Available <network>s are 'sara', 'lbr', 'swis' and 'cbi'"
+    print "Available <network>s are 'swis' and 'cbi'"
     print "------------------------------------------------------------"
    
     run_simple('0.0.0.0', 8080, combined_app, use_reloader=True,
