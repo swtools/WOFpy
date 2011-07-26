374385c717cd7a1c51fbf59cddb9cc74d1076226
Tim Whiteaker <twhit@mail.utexas.edu>
2011-07-26 13:38:10 -0500

    Added if name==main to ODM runserver example.

--- examples/odm/runserver_lbr.py
+++ examples/odm/runserver_lbr.py
@@ -8,10 +8,9 @@ import private_config
 
 logging.basicConfig(level=logging.DEBUG)
 
-
-dao = OdmDao(private_config.lbr_connection_string)
-app = wof.create_wof_app(swis_dao, 'lbr_config.cfg')
-app.config['DEBUG'] = True
-
 if __name__ == '__main__':
+    dao = OdmDao(private_config.lbr_connection_string)
+    app = wof.create_wof_app(dao, 'lbr_config.cfg')
+    app.config['DEBUG'] = True
+
     app.run(host='0.0.0.0', port=8080, threaded=True)
