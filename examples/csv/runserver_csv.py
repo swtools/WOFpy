06c5dbe2471ea90b172e728975068cee5daf7abc
Tim Whiteaker <twhit@mail.utexas.edu>
2011-08-31 14:05:21 -0500

    tiny edit to printed text in csv runserver example

--- examples/csv/runserver_csv.py
+++ examples/csv/runserver_csv.py
@@ -16,9 +16,9 @@ app = wof.create_wof_app(dao, CSV_CONFIG_FILE)
 app.config['DEBUG'] = True
 
 if __name__ == '__main__':
-    print "-----------------------------------------------------------------"
+    print "----------------------------------------------------------------"
     print "Access 'REST' endpoints at http://127.0.0.1:8080/"
-    print "Access SOAP WSDLs at http://127.0.0.1:8080/soap/wateroneflow.wsdl"
-    print "-----------------------------------------------------------------"
+    print "Access SOAP WSDL at http://127.0.0.1:8080/soap/wateroneflow.wsdl"
+    print "----------------------------------------------------------------"
 
     app.run(host='0.0.0.0', port=8080, threaded=True)
